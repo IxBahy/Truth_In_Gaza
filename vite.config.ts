334a895baa342b4cb45597f7858582541e19d81e
IxBahy <ixbahy@gmail.com>
2023-10-18 22:31:49 +0300

    fix vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
