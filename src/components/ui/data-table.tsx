6063a6dc3944199355f7eae546f38d4ec51c43b2
IxBahy <ixbahy@gmail.com>
2023-10-18 22:58:03 +0300

    fix

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -4,7 +4,6 @@ import {
 	getCoreRowModel,
 	useReactTable,
 } from "@tanstack/react-table";
-import React from "react";
 
 import {
 	Table,
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true,
-		},
-	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
