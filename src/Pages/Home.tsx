deca751ef41548a96a222a7f84021c4fdd61ad45
IxBahy <ixbahy@gmail.com>
2023-10-22 11:07:45 +0300

    edit : home desc

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,18 +6,25 @@ const Home = () => {
 		<section>
 			<div className=" text-center mt-8 mx-auto w-[360px]">
 				<img className="block" src={imgUrl} alt="" />
-				<pre className="mt-4">
-					This website is to address the truth of the Palestine and Isreal conflict
-					here you will find some threads in multiple languages that explain some facts that the West media won't tell you 
-					the goal is to show you the hypocrisy and to let you decide who is the true terror
+				<p className="mt-4">
+					This website is to address the truth of the Palestine and Isreal
+					conflict <br />
+					here you will find some threads in multiple languages that explain
+					some facts that the West media won't tell you
+					<br />
+					the goal is to show you the hypocrisy and to let you decide who is the
+					true terror <br />
 					and also some pictures of the war crimes commited by Isreal and the
 					tweets that gives context related to each photo
-					please do the right thing please 
+					<br /> please do the right thing please
+					<br />
 					💚💚SUPPORT PALESTINE 💚💚
-				</pre>
+				</p>
 			</div>
-			<h3 className="mt-4 text-red-700 text-xl text-center font-bold">
-				Think twice before supporting them!!
+			<h3 className="mt-4 text-red-700 text-md text-center font-bold">
+				if you can still support isreal to bomb gaza after checking the Isreal
+				war crimes tab i honestly encourge you to question yourself <br />
+				FOR GOD SAKE SAVE GAZA
 			</h3>
 		</section>
 	);
