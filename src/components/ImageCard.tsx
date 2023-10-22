b76788c4a9d7cf21c7129097feb1f9e89ae97d6c
IxBahy <ixbahy@gmail.com>
2023-10-22 21:19:40 +0300

    fix image card

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,16 +1,16 @@
 const ImageCard = ({ image }: { image: string }) => {
 	return (
 		<>
-			<div className=" flex flex-col justify-center min-h-[220px] max-h-[220px] rounded-xl shadow shadow-gray-500 border mx-1 border-gray-200 w-[280px] ">
+			<div className=" flex flex-col justify-center shadow-md shadow-gray-500 border mx-1 w-[280px] ">
 				<img
 					src={image}
-					className="w-[180px] max-h-[160px] lg:w-[240px] mx-auto py-2"
+					className="w-[280px] max-h-[160px] lg:w-[240px] mx-auto "
 					alt=""
 				/>
-				<span className="text-center text-xs font-bold inline-block pb-2">
-					SHOW ORIGINAL TWEET
-				</span>
 			</div>
+			<span className="text-center text-xs font-bold inline-block py-2">
+				SHOW ORIGINAL TWEET
+			</span>
 		</>
 	);
 };
