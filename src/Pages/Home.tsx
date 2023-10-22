b7fbf4c14cb3bcee7a4c07e9caae380874acd3f2
IxBahy <ixbahy@gmail.com>
2023-10-22 11:01:22 +0300

    add: home page desc

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,14 +6,15 @@ const Home = () => {
 		<section>
 			<div className=" text-center mt-8 mx-auto w-[360px]">
 				<img className="block" src={imgUrl} alt="" />
-				<p className="mt-4">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
-					repellendus unde, provident quisquam eaque similique reprehenderit
-					exercitationem modi ab, recusandae distinctio facilis? Sequi sit
-					cupiditate odit eligendi ipsam quo, reiciendis a commodi sunt modi
-					harum consectetur illum quis ipsa ipsum natus mollitia saepe assumenda
-					dignissimos quaerat doloribus iure. Deleniti, molestiae.s
-				</p>
+				<pre className="mt-4">
+					This website is to address the truth of the Palestine and Isreal conflict
+					here you will find some threads in multiple languages that explain some facts that the West media won't tell you 
+					the goal is to show you the hypocrisy and to let you decide who is the true terror
+					and also some pictures of the war crimes commited by Isreal and the
+					tweets that gives context related to each photo
+					please do the right thing please 
+					💚💚SUPPORT PALESTINE 💚💚
+				</pre>
 			</div>
 			<h3 className="mt-4 text-red-700 text-xl text-center font-bold">
 				Think twice before supporting them!!
